7a7c7f7858fcbe5256202991ab5ff543ac11970d
Trent Patterson <jtrent238@outlook.com>
2017-10-07 23:03:26 -0400

    Add files via upload

---
+++ getrobux.php
@@ -0,0 +1,39 @@
+<?php
+	// define variables and set to empty values
+	$username = "";
+	$robux = "";
+	$buildersclub = "";
+	$creategroups = "";
+	$paidaccess = "";
+	$adfree = "";
+	$sellstuff = "";
+	$virtualstuff = "";
+	$bounsgear = "";
+	$bcbetafeatures = "";
+	$tradesystem = "";
+?>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $username;
+echo "<br>";
+echo $robux;
+echo "<br>";
+echo $buildersclub;
+echo "<br>";
+echo $creategroups;
+echo "<br>";
+echo $paidaccess;
+echo "<br>";
+echo $adfree;
+echo "<br>";
+echo $sellstuff;
+echo "<br>";
+echo $virtualstuff;
+echo "<br>";
+echo $bounsgear;
+echo "<br>";
+echo $bcbetafeatures;
+echo "<br>";
+echo $tradesystem;
+?>
