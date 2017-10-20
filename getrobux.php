380ae9bb767234ee7591890d3ca48ed645e8a06a
Trent Patterson <jtrent238@outlook.com>
2017-10-20 16:45:51 -0400

    Add files via upload

--- getrobux.php
+++ getrobux.php
@@ -1,39 +1,50 @@
 <?php
-	// define variables and set to empty values
-	$username = "";
-	$robux = "";
-	$buildersclub = "";
-	$creategroups = "";
-	$paidaccess = "";
-	$adfree = "";
-	$sellstuff = "";
-	$virtualstuff = "";
-	$bounsgear = "";
-	$bcbetafeatures = "";
-	$tradesystem = "";
+// define variables and set to empty values
+$username = $robux = $buildersclub = $creategroups = $paidaccess = $adfree = $sellstuff = $virtualstuff = $bounsgear = $bcbetafeatures = $tradesystem = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  	$username = test_input($_POST["username"]);
+	$robux = test_input($_POST["robux"]);
+	$buildersclub = test_input($_POST["buildersclub"]);
+	$creategroups = test_input($_POST["creategroups"]);
+	$paidaccess = test_input($_POST["paidaccess"]);
+	$adfree = test_input($_POST["adfree"]);
+	$sellstuff = test_input($_POST["sellstuff"]);
+	$virtualstuff = test_input($_POST["virtualstuff"]);
+	$bounsgear = test_input($_POST["bounsgear"]);
+	$bcbetafeatures = test_input($_POST["bcbetafeatures"]);
+	$tradesystem = test_input($_POST["tradesystem"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 ?>
 
 <?php
 echo "<h2>Your Input:</h2>";
-echo $username;
+echo "Username: " + $username__toString();
 echo "<br>";
-echo $robux;
+echo "Robux: " + $robux;
 echo "<br>";
-echo $buildersclub;
+echo "Builders Club: " + $buildersclub__toString();
 echo "<br>";
-echo $creategroups;
+echo "Create Groups: " + $creategroups;
 echo "<br>";
-echo $paidaccess;
+echo "Paid Access: " + $paidaccess;
 echo "<br>";
-echo $adfree;
+echo "Ad Free: " + $adfree;
 echo "<br>";
-echo $sellstuff;
+echo "Sell Stuff: " + $sellstuff;
 echo "<br>";
-echo $virtualstuff;
+echo "Virtual Stuff: " + $virtualstuff;
 echo "<br>";
-echo $bounsgear;
+echo "Bouns Gear: " + $bounsgear;
 echo "<br>";
-echo $bcbetafeatures;
+echo "BC Beta Features: " + $bcbetafeatures;
 echo "<br>";
-echo $tradesystem;
+echo "Trade System: " + $tradesystem;
 ?>
